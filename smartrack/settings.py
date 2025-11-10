b862799a2af5bf2b86d99c55fa390bdb16529d59
aremandanaveenkumar <56vkbmfw@students.codeinstitute.net>
2025-11-10 07:05:24 +0000

    Update template options in settings.py with crispy_forms_tags and field in builtins

--- smartrack/settings.py
+++ smartrack/settings.py
@@ -79,6 +79,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'bag.contexts.bag_contents'
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ],
         },
     },
 ]
