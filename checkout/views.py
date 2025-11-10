1223fa26e45f7027b89fe3d867a3d635a6ecfd19
aremandanaveenkumar <56vkbmfw@students.codeinstitute.net>
2025-11-10 08:34:06 +0000

    Add stripepublic key and test client secret

--- checkout/views.py
+++ checkout/views.py
@@ -12,6 +12,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51SRpYYDVNX1aJ6hcaNm97IrlBRFfvndbTIXoLzYC7VpHif9CQxFUU6kdJEtrdPZAu1SFxniUEJJCxKwbqY5uPGn900B0IlaTIE',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
