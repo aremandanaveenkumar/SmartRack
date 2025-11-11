8e80b8b3b884e6bb153a3faa009b043fc2e5dec3
aremandanaveenkumar <56vkbmfw@students.codeinstitute.net>
2025-11-11 13:58:37 +0100

    Update state in address for stripe in stripe_elements.js

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -70,6 +70,7 @@ form.addEventListener('submit', function (ev) {
                         line2: $.trim(form.street_address2.value),
                         city: $.trim(form.town_or_city.value),
                         country: $.trim(form.country.value),
+                        state: $.trim(form.state.value),
                     }
                 }
             },
@@ -82,6 +83,7 @@ form.addEventListener('submit', function (ev) {
                     city: $.trim(form.town_or_city.value),
                     country: $.trim(form.country.value),
                     postal_code: $.trim(form.postcode.value),
+                    state: $.trim(form.state.value),
                 }
             },
         }).then(function(result) {
