14821759ff156e5b011f2664bcbeeccc7c92f78c
aremandanaveenkumar <56vkbmfw@students.codeinstitute.net>
2025-11-13 07:54:34 +0100

    Fix duplication error in settings installed_apps

--- smartrack/settings.py
+++ smartrack/settings.py
@@ -36,7 +36,7 @@ ALLOWED_HOSTS = [
 
 # Application definition
 
-INSTALLED_APPS = [
+INSTALLED_APPS = [    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -51,8 +51,7 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
-    'checkout',
-    'profiles',    
+    'profiles',
     'crispy_forms',
 ]
 
