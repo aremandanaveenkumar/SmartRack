fec52952b00f82f5742a49fc05f8e08357119658
aremandanaveenkumar <56vkbmfw@students.codeinstitute.net>
2025-11-21 08:44:46 +0100

    revert back settings

--- smartrack/settings.py
+++ smartrack/settings.py
@@ -201,7 +201,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -209,7 +209,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 if 'USE_AWS' in os.environ:
     AWS_STORAGE_BUCKET_NAME = 'smart-rack-s3-bucket'
     AWS_S3_REGION_NAME = 'eu-north-1'
-    AWS_DEFAULT_ACL = 'None'   
+    AWS_DEFAULT_ACL = 'public-read'   
 
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
